77257d5f500ca8626e43a8877d92602b1b776758
pehawn <patrickhawn8331@gmail.com>
2023-06-22 00:01:00 -0500

    initial commit take two

---
+++ src/types/IAudio.ts
@@ -0,0 +1,19 @@
+import { IStem } from "./IStem";
+
+export interface IAudio {
+	Path: string;
+	Name: string;
+	FullName?: string;
+	Duration?: number;
+	CurrentTimestampEventId?: number;
+	Paused?: boolean;
+	Reversed?: boolean;
+	Looped?: boolean;
+	LoopStart?: number;
+	LoopEnd?: number;
+	Bpm?: number;
+	Stems?: IStem[];
+	IsHover?: boolean;
+	AlbumArt?: any;
+	CardColor?: string;
+}
